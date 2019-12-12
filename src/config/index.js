fc27fc4b349c6c24295dda0e9fb8aea99747ed47
Fang Han <hanfangviolin@gmail.com>
2019-12-12 11:51:48 -0500

    added google analytics

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@ module.exports = {
   siteKeywords: 'Fang Cabrera, TakaiKinoko, software engineer, NYU, Courant',
   siteUrl: 'https://fangcabrera.com',
   siteLanguage: 'en_US',
-  //googleAnalyticsID: 'UA-45666519-2',
+  googleAnalyticsID: 'G-Y1DV8CC1F8',
   //googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
   name: 'Fang Cabrera',
   location: 'New York, NY',
